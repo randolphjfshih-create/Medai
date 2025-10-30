a82c0b7fadbb9fa2ea02078fb742b35c4cf2fb44
randolphjfshih-create <randolphjfshih@gmail.com>
2025-10-30 20:34:11 +0800

    Delete src directory

--- src/utils/logger.ts
+++
@@ -1,9 +0,0 @@
-export function logInfo(tag: string, data: any) {
-  console.log(`ℹ️ [${tag}]`, data);
-}
-export function logWarn(tag: string, data: any) {
-  console.warn(`⚠️ [${tag}]`, data);
-}
-export function logError(tag: string, data: any) {
-  console.error(`❌ [${tag}]`, data);
-}
